c7461629ace1355140d0a66e7afca1c0e5e18680
Heritier Moise <vitalheritier1@gmail.com>
2026-07-07 12:23:03 +0300

    correction

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,29 @@
 // Service API principal
 import { API_BASE_URL } from '../config/api';
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  },
+  timeout: 30000, // 30 secondes
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('auth_token');
+
+  if (token) {
+    config.headers = config.headers ?? {};
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+export default api;
+
 
 // Types
 export interface User {
